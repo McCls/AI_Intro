3444adcd26c0f205cc78acd04444f53d44ad574c
AnAspiringCoder <cmcclure17@gmail.com>
2016-08-30 09:13:27 +0000

    Final file renaming and jasmine initialization.

---
+++ bruteTSP/brute.js
@@ -0,0 +1,23 @@
+// http://stackoverflow.com/questions/9960908/permutations-in-javascript
+
+  var permArr = [],
+  usedChars = [];
+
+function permute(input) {
+
+  var i, ch;
+  for (i = 0; i < input.length; i++) {
+    ch = input.splice(i, 1)[0];
+    usedChars.push(ch);
+    if (input.length == 0) {
+      permArr.push(usedChars.slice());
+    }
+    permute(input);
+    input.splice(i, 0, ch);
+    usedChars.pop();
+  }
+  return permArr
+};
+
+
+console.log(JSON.stringify(permute([5, 3, 7, 1])));
